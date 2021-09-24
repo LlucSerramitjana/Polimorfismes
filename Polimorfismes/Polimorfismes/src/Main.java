cea250c414565ba8a9e86cf987aa6f5f4e79c4e6
LlucSerramitjana <80621909+LlucSerramitjana@users.noreply.github.com>
2021-09-24 12:07:59 +0200

    Add files via upload

---
+++ Polimorfismes/Polimorfismes/src/Main.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static double suma (Figures[] v){
+        double res=0;
+        for(Figures f:v){
+            res+=f.area();
+        }
+        return res;
+    }
+
+    public static void main (String[] args){
+        Rectangles rectangle = new Rectangles(3,5);
+        Cercles cercle = new Cercles(7);
+        Triangle triangle = new Triangle (1,2.5);
+        Quadrat quadrat = new Quadrat(6);
+
+        Figures[] figures = {rectangle, cercle, triangle, quadrat};
+        for (Figures figura : figures){
+            System.out.println("Area:"+figura.area());
+        }
+        System.out.println(suma(figures));
+        ArrayList<Figures> figura1 = new ArrayList<>();
+        figura1.add(rectangle);
+        figura1.add(cercle);
+        figura1.add(triangle);
+        figura1.add(quadrat);
+        Collections.sort(figura1);
+        for(Figures f : figura1){
+            System.out.println(f.area());
+        }
+    }
+}
